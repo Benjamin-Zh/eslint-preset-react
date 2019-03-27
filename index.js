b25f56abd37c411643b4a585f746f8b046429026
zhangfeng <zhangfeng@wifi.com>
2019-03-27 15:44:52 +0800

    feat: first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = () => ({
+  extends: ['airbnb-base'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    allowImportExportEverywhere: true,
+  },
+  env: {
+    browser: true,
+  },
+
+  // 解析 webpack alias 配置
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: '@guys/william/configs/index.js'
+      },
+    },
+  },
+
+  rules: {
+
+    // 允许 devDependencies
+    'import/no-extraneous-dependencies': [
+      'error',
+      { "devDependencies": true }
+    ],
+
+  },
+});
